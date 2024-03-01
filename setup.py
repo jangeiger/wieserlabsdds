3c3fd959e5da329d3a9bf638a4b585e74d7b5a25
jangeiger <62647585+jangeiger@users.noreply.github.com>
2024-03-01 18:01:47 +0100

    setup package installer

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+   name="wieserlabsdds",
+   version="0.1",
+   description="A python API to control the Wieserlabs DDS",
+   packages=["wieserlabsdds"],  #same as name
+)
